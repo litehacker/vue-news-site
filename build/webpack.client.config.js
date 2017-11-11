35815b6fcccb30ca5e697a11f59630cc96a41238
giorgi94 <giorgik1994@gmail.com>
2017-11-11 10:37:54 +0400

    init project

---
+++ build/webpack.client.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.config.js')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const PATH = path.join(__dirname, '..')
+
+
+var entry = {
+    main: './src/js/main.js',
+}
+
+
+var clientConfig = merge(baseConfig, {
+    context: PATH,
+    name: 'client',
+    target: 'web',
+    entry: Object.assign({
+        vendors: ['babel-polyfill', 'axios'],
+        vue: ['vue', 'vuex', 'vue-router']
+    }, entry),
+    output: {
+        path: path.join(PATH, '..', 'static'),
+        publicPath: '/static/',
+        filename: 'js/[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': "vue/dist/vue.esm.js",
+            '@src': path.join(PATH, 'src'),
+            '@static': path.join(PATH, 'static'),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
+            {
+                test: /\.coffee$/,
+                use: 'coffee-loader',
+                exclude: /node_modules/
+            }
+            
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['vue', 'vendors']
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons'
+            chunks: Object.keys(entry)
+        }),
+        new ExtractTextPlugin("css/styles.css"),
+        
+    ],
+})
+
+
+
+
+module.exports = clientConfig;
