ae88a7d0b862a5988e1320b26179120d68c0e7be
admin <admin@localhost.localdomain>
2021-03-11 21:40:06 +0400

    mega menu update - header

--- frontend/components/HeaderMenu.vue
+++ frontend/components/HeaderMenu.vue
@@ -1,24 +1,45 @@
 <template>
-   <div class="headerMenu container-md">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">-LOGO-</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-for="(item, index) in headerMenuLinks" :key="index">
-                        <li class="nav-item p-1 "  >
-                            <router-link class="nav-link " :to="item.url">
-                                {{ item.title }}
-                            </router-link>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
+    <div class=" container-flux">
+        <nav class="navbar navbar-expand-lg navbar-light  border">
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0" v-for="(item, index) in headerMenuLinks" :key="index">
+                    <li class="nav-item p-1 "  >
+                        <router-link class="nav-link " :to="item.url">
+                            {{ item.title }}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div class="  px-0" style="background-color:#042b40">
+
+       <div style="height:150px" class="container-xl position-relative">
+                           <a class="navbar-brand align-middle" href="#">-LOGO-</a>
+
+                    <form class="d-flex position-absolute bottom-0 end-0 py-2">
                         <input class="form-control me-2" type="search" placeholder="ჩაწერე საძიებო სიტყვა" aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">ძებნა</button>
                     </form>
-                </div>
+
+       </div>
+        <nav class="navbar navbar-expand-lg navbar-dark container-xl d-flex">
+            <div class="collapse navbar-collapse d-flex justify-content-evenly">
+                <ul class="navbar-nav flex-row" v-for="(item, index) in headerMenuLinks" :key="index">
+                    <li class="nav-item p-1 border to-hover ">
+                        <router-link class="nav-link dropdown-toggle" data-bs-toggle="dropdown" :to="item.url">
+                            {{ item.title }}
+                        </router-link>
+                        <ul class="to-show position-absolute bg-light ">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                    
+                </ul>
             </div>
         </nav>
     </div>
@@ -37,25 +58,90 @@ export default defineComponent({
         return {
             headerMenuLinks: [
                 {
-                    title:"მთავარი",
+                    title:"საზოგადოება",
                     url:"/",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
                 },
                 {
-                    title:"მეორეხარისხოვანი",
+                    title:"ეკონომიკა",
                     url:"/2",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
                 },
                 {
-                    title:"კონტაქტი",
+                    title:"მსოფლიო",
                     url:"/3",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
                 },
                 {
-                    title:"ებაუთ",
+                    title:"განათლება",
                     url:"/4",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
                 },
                 {
-                    title:"მთავარი",
+                    title:"HI-Tech",
                     url:"/5",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
                 },
+                {
+                    title:"კულტურა",
+                    url:"/5",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
+                },
+                {
+                    title:"მოგზაურობა",
+                    url:"/5",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
+                },
+                {
+                    title:"საინტერესო",
+                    url:"/5",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
+                },
+                {
+                    title:"ყველა",
+                    url:"/5",
+                    subCategories:[
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                        {title:"სათაური",imageUrl:"/static/images/dummy_slide.jpg"},
+                    ]
+                },
+
             ]
         }
     }
--- frontend/views/Home.vue
+++ frontend/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
    <div>
-        <TopSlider />
+        <!-- <TopSlider /> -->
    </div>
 </template>
 
