ad9d475ad393d50ae39862a7eb369af0358ad084
Giorgi Gvimradze <natceo@gmail.com>
2021-03-18 15:00:44 +0400

    search & menu more>

--- frontend/components/HeaderMenu.vue
+++ frontend/components/HeaderMenu.vue
@@ -60,7 +60,7 @@
                             {{ item.title }}
                         </router-link>
 
-                        <div class="to-show py-3 px-0 bg-dark vw-100 start-50 translate-middle-x position-absolute top-100">
+                        <div class="to-show py-3 pb-5 px-0 bg-dark vw-100 start-50 translate-middle-x position-absolute top-100">
                             <div class="d-flex container p-0 mb-0">
                                 <div class="row justify-content-start col-10">
                                     <li
@@ -77,24 +77,22 @@
                                                     :src="subCats.imageUrl"
                                                     class="card-img-top"
                                                     :alt="subCats.title"
+                                                    style="max-height:150px"
                                                 />
+                                            
+                                                <div class="card-body position-absolute bg-dark col-10" style="top:100px; min-height:90px; max-height:90px">
+                                                    <h6 class="card-title">
+                                                        {{ subCats.title }}
+                                                    </h6>
+                                                </div>
                                             </router-link>
-                                            <div class="card-body position-absolute bg-dark col-10" style="top:100px; ">
-                                                <h5 class="card-title">
-                                                    {{ subCats.title }}
-                                                </h5>
-                                                <p class="card-text">
-                                                    Some quick example text to build
-                                                    on the card title and make up
-                                                    the bulk of the card's content.
-                                                </p>
-                                            </div>
                                         </div>
                                     </li>
                                 </div>
-                                <div class="col-2">
-                                    <router-link class="text-decoration-none" :to="item.url">
-                                         <span style="font-size:30">მეტი > </span>
+                                <div class="col-2 position-absolute bottom-0 pb-3" style="right: 50px;">
+                                    <router-link class="text-decoration-none" :to="item.url" style="color:white">
+                                        მეტი    
+                                         <svg width="32" height="32" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6.646 3.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L12.293 10 6.646 4.354a.5.5 0 010-.708z"/></svg>
                                     </router-link>
                                 </div>
                             </div>
@@ -315,7 +313,7 @@ export default defineComponent({
                             url: "/subcat",
                         },
                         {
-                            title: "სათაური 30",
+                            title: "სათაური 30 სათაური 30 სათაური 30 სათაური 30",
                             imageUrl: "/static/images/dummy_slide.jpg",
                             url: "/subcat",
                         },
